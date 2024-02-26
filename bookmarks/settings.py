c0b8a15a5eba6e7907595ff659ed0273b2c54438
thapelomagqazana <tapsmcgzee8@gmail.com>
2024-02-27 01:08:20 +0200

    Modified settings

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1', 'web-production-a127.up.railway.app']
 
 
 # Application definition
