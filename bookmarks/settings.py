be7f549f78b84b48bdeda8a1b3c4f035f7a5c7b5
thapelomagqazana <tapsmcgzee8@gmail.com>
2024-02-27 01:14:12 +0200

    Added a host on settings

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -32,6 +32,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
+#
+
 ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1', 'web-production-a127.up.railway.app']
 
 
