05c6ac49e85e168808b5c3a8c241b9ff38eaaacd
thapelomagqazana <tapsmcgzee8@gmail.com>
2024-02-27 16:38:17 +0200

    Modified settings.py

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -143,7 +143,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = os.path.join(BASE_DIR, "app/staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -156,7 +156,7 @@ LOGOUT_URL = "logout"
 
 MEDIA_URL = "media/"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "app/media")
+MEDIA_ROOT = BASE_DIR / "media"
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
